651eeb64c69546a10280a3e6739e65b68975954e
Demanikys <snopok.stanislav@mail.ru>
2020-09-29 16:47:38 +0300

    feat: add max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  var result = 0;
+  var i = 0;
+  if ((typeof (array) === 'object') && (array !== '')) {
+    for (i; i < array.length; i++) {
+      if (result > array[i]) {
+        result = array[i];
+      };
+    };
+  } else {
+    result = 0;
+  };
+  return result;
+};
+
+exports.max = function max(array) {
+  var result = 0;
+  var i = 0;
+  if ((typeof (array) === 'object') && (array !== '')) {
+    for (i; i < array.length; i++) {
+      if (result < array[i]) {
+        result = array[i];
+      };
+    };
+  } else {
+    result = 0;
+  };
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  var result = 0;
+  var i = 0;
+  if ((typeof (array) === 'object') && (array != '')) {
+    for (i; i < array.length; i++) {
+      result = result + array[i];
+    }
+    result = result / array.length;
+  } else {
+    result = 0;
+  };
+  return result;
 }
